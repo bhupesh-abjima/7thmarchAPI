e2cb300a9132e1b8ac074c6713af8bbd2c75432f
bhupesh-abjima <97227019+bhupesh-abjima@users.noreply.github.com>
2022-03-07 21:14:43 +0530

    Add files via upload

---
+++ ICampRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TheCodeCamp.Data
+{
+  public interface ICampRepository
+  {
+        Task<bool> SaveChangesAsync();
+
+  
+    void AddCamp(Camp camp);
+    void DeleteCamp(Camp camp);
+    Task<Camp[]> GetAllCampsAsync(bool includeTalks = false);
+    Task<Camp> GetCampAsync(string moniker, bool includeTalks = false);
+    Task<Camp[]> GetAllCampsByEventDate(DateTime dateTime, bool includeTalks = false);
+
+   
+    void AddTalk(Talk talk);
+    void DeleteTalk(Talk talk);
+    Task<Talk> GetTalkByMonikerAsync(string moniker, int talkId, bool includeSpeakers = false);
+    Task<Talk[]> GetTalksByMonikerAsync(string moniker, bool includeSpeakers = false);
+
+   
+    void AddSpeaker(Speaker speaker);
+    void DeleteSpeaker(Speaker speaker);
+    Task<Speaker[]> GetSpeakersByMonikerAsync(string moniker);
+    Task<Speaker> GetSpeakerAsync(int speakerId);
+    Task<Speaker[]> GetAllSpeakersAsync();
+
+  }
+}
